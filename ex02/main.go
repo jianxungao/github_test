a85048c99a93508c38cab6a5dacdfe5cdcf45aff
Jianxun Gao <gao_jianxun@hotmail.com>
2019-04-16 19:13:54 +0800

    add ex02

--- ex02/main.go
+++ ex02/main.go
@@ -16,13 +16,33 @@ func incr(p *int) int {
 	return *p
 }
 
+func bitshift(v *int) int {
+	return *v << 1
+}
+
+func bitshiftu(v int) int {
+	return v << 2
+}
+
 func main() {
 
 	x := 1
 	p := &x         // p, of type *int, points to x
 	fmt.Println(*p) // "1"
 	*p = 2          // equivalent to x = 2
-	fmt.Println(x)  // "2"
+	fmt.Printf("%x\t%b\t%d\n", x, x, x)
+
+	c := bitshiftu(x)
+	fmt.Printf("%x\t%b\t%d\n", c, c, c)
+	fmt.Println("now x is ", x)
+
+	b := bitshift(p)
+	fmt.Printf("%x\t%b\t%d\n", b, b, b)
+	fmt.Println("now x is ", x)
+
+	d := *p << 1
+	fmt.Printf("%x\t%b\t%d\n", d, d, d)
+	fmt.Println("now x is ", x)
 
 	var x2, y2 int
 	fmt.Println(&x2 == &x2, &x2 == &y2, &x2 == nil) // "true false false"
