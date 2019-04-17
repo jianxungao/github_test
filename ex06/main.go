4c4f8bdd92af87b4a1da39b57b5546921fc5dc38
Jianxun Gao <gao_jianxun@hotmail.com>
2019-04-17 13:33:00 +0800

    update

--- ex06/main.go
+++ ex06/main.go
@@ -14,32 +14,36 @@ type agent struct {
 	ltk bool
 }
 
+//interface any type has speak method, it is human
 type human interface {
 	speak()
 }
 
+// polymorphism
 func bar(h human) {
 	fmt.Println("I was passed into bar", h)
 }
 
+// method of agent
 func (a agent) speak() {
 	fmt.Println("I am", a.first, a.last, " - the agent speak")
 }
 
+// method of person
 func (p person) speak() {
 	fmt.Println("I am", p.first, p.last, " - the person speak")
 }
 
 func main() {
 	agent007 := agent{
-		person: person{
+		person: person{ //composite literal
 			"james",
 			"bone",
 		},
 		ltk: true,
 	}
 
-	person1 := person{
+	person1 := person{ //composite literal
 		"Mr:",
 		"tommorrow",
 	}
